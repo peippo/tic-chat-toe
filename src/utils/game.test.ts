d50342de8d5418d35e5b1b347589f285ac1205a7
Janne Peippo <peippo@gmail.com>
2023-04-21 10:21:15 +0300

    Add util for winning line check

---
+++ src/utils/game.test.ts
@@ -0,0 +1,65 @@
+import { describe, expect, it } from "vitest";
+import * as utils from "./game";
+
+describe("findRandomFreeCell", () => {
+  it("should return a free cell if possible", () => {
+    const occupiedCells = [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+    ];
+    const result = utils.findRandomFreeCell(occupiedCells);
+    expect(result).toBeDefined();
+    expect(result).not.toBeNull();
+    expect(result?.x).toBeDefined();
+    expect(result?.y).toBeDefined();
+    expect(result?.x).toBeGreaterThanOrEqual(0);
+    expect(result?.x).toBeLessThan(3);
+    expect(result?.y).toBeGreaterThanOrEqual(0);
+    expect(result?.y).toBeLessThan(3);
+    expect(
+      occupiedCells.some(
+        (occupiedCell) =>
+          occupiedCell.x === result?.x && occupiedCell.y === result?.y
+      )
+    ).toBeFalsy();
+  });
+
+  it("should return undefined when all cells are occupied", () => {
+    const occupiedCells = [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+      { x: 0, y: 2 },
+      { x: 1, y: 2 },
+      { x: 2, y: 2 },
+    ];
+    const result = utils.findRandomFreeCell(occupiedCells);
+    expect(result).toBeUndefined();
+  });
+});
+
+describe("playerHasWinningLine", () => {
+  it("should return true if the player has a winning line", () => {
+    const playerTurns = [
+      { x: 0, y: 0 },
+      { x: 1, y: 1 },
+      { x: 2, y: 2 },
+    ];
+    expect(utils.playerHasWinningLine(playerTurns)).toBe(true);
+  });
+
+  it("should return false if the player does not have a winning line", () => {
+    const playerTurns = [
+      { x: 0, y: 0 },
+      { x: 1, y: 1 },
+      { x: 0, y: 2 },
+    ];
+    expect(utils.playerHasWinningLine(playerTurns)).toBe(false);
+  });
+});
--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,3 +1,4 @@
+import type { Turn } from "@prisma/client";
 import type { Cell } from "~/types";
 
 export const findRandomFreeCell = (occupiedCells: Cell[]): Cell | undefined => {
@@ -23,4 +24,40 @@ export const findRandomFreeCell = (occupiedCells: Cell[]): Cell | undefined => {
   const randomIndex = Math.floor(Math.random() * freeCells.length);
 
   return freeCells[randomIndex];
+};
+
+/* prettier-ignore */
+const winLines = [
+  // horizontal lines
+  [ {x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0} ],
+  [ {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1} ],
+  [ {x: 0, y: 2}, {x: 1, y: 2}, {x: 2, y: 2} ],
+  
+  // vertical lines
+  [ {x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2} ],
+  [ {x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: 2} ],
+  [ {x: 2, y: 0}, {x: 2, y: 1}, {x: 2, y: 2} ],
+
+  // diagonal lines
+  [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2} ],
+  [ {x: 0, y: 2}, {x: 1, y: 1}, {x: 2, y: 0} ]
+]
+
+export const playerHasWinningLine = (playerTurns: Pick<Turn, "x" | "y">[]) => {
+  let foundWinningLine = false;
+
+  for (const line of winLines) {
+    if (
+      line.every((cell) =>
+        playerTurns.some(
+          (playedCell) => playedCell.x === cell.x && playedCell.y === cell.y
+        )
+      )
+    ) {
+      foundWinningLine = true;
+      break;
+    }
+  }
+
+  return foundWinningLine;
 };
